4812a058ffdf43c04ff20c42ab21ce4d30d7380b
Андреев Владимир <vzlobot01@mail.ru>
2019-02-22 12:07:30 +0200

    Rename folders

---
+++ SmartKitchen.DB/dbo/Tables/Products.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (MAX) NOT NULL,
+    [Category] INT            NOT NULL,
+    CONSTRAINT [PK__Products__3214EC072DC8B59A] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Products_Categories] FOREIGN KEY ([Category]) REFERENCES [dbo].[Categories] ([Id])
+);
+
+
+
+
+
